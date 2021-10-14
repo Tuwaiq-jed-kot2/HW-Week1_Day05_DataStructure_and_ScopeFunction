7648f4c997c19206d98e5f155e719439dcb0c2c0
shuruqt1000 <92253616+shuruqt1000@users.noreply.github.com>
2021-10-14 16:19:47 +0300

    Update assignment.md

--- src/solution_part2.kt
+++ src/solution_part2.kt
@@ -1,3 +1,32 @@
 fun main(){
 
+val Animals = mutableListOf("cat","dog","rabbit")
+println(Animals)
+
+    Animals.add("fish")
+println(Animals)
+
+
+
+
+val N = listOf(3,5,7,9)
+
+try {
+    println(N[2])
+}catch (e: ArrayIndexOutOfBoundsException){
+    println("index out of range")
 }
+
+N.getOrElse(2){
+    println("index out of range")
+}
+
+println(N.getOrNull(2))
+
+
+
+val Y = mutableListOf(2,2,4,3,3,3,9).toMutableSet()
+println(Y)
+    Y.add(5)
+    println(Y)
+
--- src/solution_part3.kt
+++ src/solution_part3.kt
@@ -1,3 +1,43 @@
-fun main(){
+/*
+There are many uses such as hospitals when they want to link
+ the names of patients with the file number, so it is easy to identify them with the system
+  and send reminders of their appointments
+ */
 
+/*
+Immutaple is a variable cann't be changed
+ read only is a variable cann't be changed after initalization and cann't be reset
+ */
+
+
+
+
+
+
+
+
+fun main(){
+    val programmers= mapOf(
+        1.1 to "Bayan",
+        2.2 to "Ashwag",
+        3.3 to "Nedaa",
+        4.4 to "Asmaa",
+        5.5 to "Ohood",
+        6.6 to "sumaya",
+        7.7 to "Hajer",
+        8.8 to "Jumana",
+        9.9 to "Kayan",
+        10.10 to "Wejdan",
+        11.11 to "Raghd",
+        12.12 to "Shahad",
+        13.13 to " Rahaf",
+        14.14 to " Nouf",
+        15.15 to " Reem",
+        16.16 to " Ruba",
+        17.17 to "Sara",
+        18.18 to "Alaa",
+        19.19 to " Nada",
+        20.20 to " Mona"
+    )
+println(programmers)
 }
