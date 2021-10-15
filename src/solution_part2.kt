2cbad33a124168b0ea02b9f5d586c5a0cd9fe76c
Qussai <qussai.Saeed.jed.k2@tuwaiq.edu.sa>
2021-10-15 15:34:12 -0400

    i like it

--- src/solution_part2.kt
+++ src/solution_part2.kt
@@ -1,3 +1,27 @@
-fun main(){
+fun main() {
+    val num = mutableListOf(1,2,3,4,5,6)
+    println(num)
+    num.getOrElse(8){
+        println("write right number")
+    }
+    println(num.getOrNull(9))
 
+}
+
+fun main() {
+    val names = mutableListOf("Qussai", "ali","ahmad")
+    println(names)
+    names += "hani"
+    println(names)
+    names.add(2,"assaf")
+    println(names)
+
+
+
+}
+
+fun main() {
+    val numberss = mutableListOf(1,2,3,4,5,5,6,7,7,8,8,8,6).toMutableSet()
+    numberss.add(9)
+    println(numberss)
 }
--- src/solution_part3.kt
+++ src/solution_part3.kt
@@ -1,3 +1,34 @@
-fun main(){
+/*- Write an asnwer to this question: What are the cases that I need to use the map for??
+we use map in Id to names ,signup email with usernames ,student number tp name , mobile number to names
+
+- What is the difference between immutable and read only variables?
+ReadOnlyCollection will prevent being modified
+ImmutableList is a Persistent Data Structure. It will return a new instance of a list with the updated values, but the original list is intact
+- Create a map of 20 elements of strings and doubles*/
+fun main() {
+
+
+    val country = mutableMapOf(
+        "Saudi" to 966,
+        "kwait" to 995,
+        "lebnon" to 994,
+        "bahrin" to 993 ,
+        "yamen" to 992 ,
+        "oman" to 991,
+        "iraq" to 990 ,
+        "amrica" to 997,
+        "france" to 998,
+        "belgum" to 999,
+        "turky" to 981,
+        "sudan" to 982,
+        "moroco" to 983,
+        "tunsia" to 984,
+        "india" to 985,
+        "pakstan" to 986,
+        "china" to 987,
+        "afghanstan" to 988 ,
+        "indonisia" to 989,
+        "sengapor" to 980,)
+    println(country)
 
 }
