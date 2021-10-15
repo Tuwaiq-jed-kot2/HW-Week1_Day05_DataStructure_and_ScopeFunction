9dff01e3a86d55792b9db273c288430b50216c86
HumaidALKhathami <humaid.ali.jed.k2@tuwaiq.edu.sa>
2021-10-15 13:22:56 +0300

    great homework, sorry my solution is not impressive but I feel a bit tired

--- src/solution_part2.kt
+++ src/solution_part2.kt
@@ -1,8 +1,13 @@
 fun main(){
+    // part 1
+
     val listOne = mutableListOf(1,2,3,4)
 
     listOne.add(5)
     println(listOne)
+
+    // part 2
+
 
     val listTwo = listOf(5,6,7,8)
 
@@ -16,9 +21,12 @@ fun main(){
         println("index out of range")
     }
 
-    listTwo.getOrNull(4)
+    println(listTwo.getOrNull(4))
 
-    var listThree = mutableListOf(1,1,1,2,2,3,4,5,6).toMutableSet()
+    // part 3
+
+
+    val listThree = mutableListOf(1,1,1,2,2,3,4,5,6).toMutableSet()
 
     listThree.add(6)
 
