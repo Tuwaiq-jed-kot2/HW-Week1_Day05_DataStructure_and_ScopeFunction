fff59973241c069583d669f651122615dca74c11
Nayef Saeed Alzahrani <NAYEF.SAEED.jed.k2@tuwaiq.edu.sa>
2021-10-15 23:51:14 +0300

    HW-Week1_Day05_DataStructure_and_ScopeFunction

--- src/solution_part1.kt
+++ src/solution_part1.kt
@@ -1,3 +1,29 @@
-fun main(){
+/*------------------------------Answers Part I-------------------------------------------*/
+/*---------------------------------Answer:Q1----------------------------------------------*/
+//Answer :Let
+//fun main(){
+//    var str = "Hi Everyone"
+//    str.let { println("$it!!") }
+//}
 
+
+/*---------------------------------Answer:Q2----------------------------------------------*/
+//Answer :Run
+//fun main() {
+//    var tutorial = "This is Kotlin Tutorial"
+//    println(tutorial) //This is Kotlin Tutorial
+//    tutorial = run {
+//        val tutorial = "This is run function"
+//        tutorial
+//    }
+//    println(tutorial) //This is run function
+//}
+
+
+/*---------------------------------Answer:Q3----------------------------------------------*/
+//Answer :Also
+fun main() {
+    var m = 1
+    m = m.also { it + 1 }.also { it + 1 }
+    println(m) //prints 1
 }
