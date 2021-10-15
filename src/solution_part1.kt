a1973299243b7bbb7276e6b448471c3c68c6886b
irakan4 <irakan4>
2021-10-15 21:19:35 +0300

    done :)

--- src/solution_part1.kt
+++ src/solution_part1.kt
@@ -1,4 +1,52 @@
 
-fun main(){
+class MultiportService(var url: String, var port: Int) {
+    fun prepareRequest(): String = "Default request"
+    fun query(request: String): String = "Result for query '$request'"
+}
 
+fun main() {
+    // let e.x
+    val numbers = mutableListOf("one", "two", "three", "four", "five")
+    numbers.map { it.length }.filter { it > 3 }.let {
+        println(it)
+    }
+
+    val nullAbleString: String? = null
+    nullAbleString?.let{
+        println(it)
+    }
+
+    if(nullAbleString == null){
+        println(nullAbleString)
+
+    }
+
+    else{
+        println("let it go :)")
+    }
+
+
+    // also
+
+    val numberz = mutableListOf("one", "two", "three")
+    numberz
+        .also { println("before adding new one: $it") }
+
+        .add("four")
+    println(numberz)
+
+
+
+
+    /// run
+
+
+    val service = MultiportService("https://google.com", 80)
+
+    val result = service.run {
+        port = 81
+        query(prepareRequest() + " to port $port")
+    }
+
+    println(result)
 }
