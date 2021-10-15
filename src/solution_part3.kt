278c98788177d3dcb54fe2d89512f08212a396c0
shahad197 <shahad.yousef.jed.k2@tuwaiq.edu.sa>
2021-10-15 18:31:05 +0300

    ;)

--- src/solution_part3.kt
+++ src/solution_part3.kt
@@ -1,3 +1,13 @@
 fun main(){
+    /*
+    for example, at university it put ID number for each student to identify them in the system
+     */
+
+    /*
+    immutable: object cannot be changed once created no matter what happens
+    read only: object that does not expose any way to change it
+     */
+
+    val x = mapOf("anas"to 2.3,"ahmed"to 2.4,"asaad"to 2,5"abrar"to 2.5"asmaa"to 2.6"asrar"to 2.7"afrah"to 2.8"afnan"to 2.9"areej"to 3.1"anfal"to 3.2"amna"to 3.3"aghsan"to 3.4"shahad"to 3.5"amal"to 3.6"ahad"to 3.7"yousef"to 3.8"zainb"to 3.9"jehad"to 4.1"marwan"to 4.2"nadin"to 4.3)
 
 }
