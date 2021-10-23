08ba1d74cf4e97277f2bce9f4d8529f996d365c1
AsrarAlbishri <Asrar.Mohammad.jed.k2@tuwaiq.edu.sa>
2021-10-24 01:52:54 +0300

    .

--- src/solution_part1.kt
+++ src/solution_part1.kt
@@ -1,6 +1,13 @@
 fun main(){
 
+    //part 1 (scope function)
 
+    //let
+
+    val str = "Hello World"
+    str.let {
+        println("$it ")
+    }
 
 
     // also
@@ -11,4 +18,21 @@ fun main(){
         it.remove(2)
     }
     println(list)
+
+
+    // run
+    val info = Student().run {
+        name = " Asrar"
+        val age1 = age+25
+        println(age1)
+        name
+    }
+  println(info)
 }
+
+
+class Student(){
+    var name = " "
+    var age = 1
+}
+
