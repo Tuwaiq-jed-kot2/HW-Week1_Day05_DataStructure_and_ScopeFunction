10af10b5e0b3ef2aadf6752df49e5943c084eb30
Nayef Saeed Alzahrani <NAYEF.SAEED.jed.k2@tuwaiq.edu.sa>
2021-10-24 03:58:31 +0300

    solved

--- src/solution_part1.kt
+++ src/solution_part1.kt
@@ -1,3 +1,25 @@
-fun main(){
-
+class Person() {
+    var name = "Naif"
+    var age = 30
 }
+
+fun main(){
+var name:String?="Naif"
+
+    fun doIt(){
+        name = null
+    }
+
+    val newName = name?.let { val new = name + " Saeed"
+        new }// Let
+    println(newName)
+
+    val person = Person()
+    person.also { it.name="Naif Saeed Alzahrani"
+    println("The new name is :${it.name}")}//Also
+
+    val newAge= Person().run { val new = age+3
+    age+3}//run
+    println(newAge)
+}
+
